80471b19d55f3c3707150836f58063d0f95c28c1
nayem567 <inew2732@gmail.com>
2024-09-30 02:33:22 +0600

    Products Added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import LoginSignup from "./Pages/LoginSignup";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import { Footer } from "./Components/Footer/Footer";
+import banner_mens from "./assets/banner_mens.png";
+import banner_women from "./assets/banner_women.png";
+import kids_banner from "./assets/banner_kids.png";
 
 const App = () => {
   return (
@@ -15,11 +18,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Shop />} />
 
-        <Route path="/men" element={<ShopCategory category="mens" />} />
+        <Route path="/men" element={<ShopCategory category="men" banner ={banner_mens} />} />
 
-        <Route path="/women" element={<ShopCategory category="women" />} />
+        <Route path="/women" element={<ShopCategory category="women" banner={banner_women} />} />
 
-        <Route path="/kids" element={<ShopCategory category="kids" />} />
+        <Route path="/kids" element={<ShopCategory category="kid" banner={kids_banner} />} />
 
         <Route path="/product" element={<Product />}>
           <Route path=":productId" element={<Product />} />
